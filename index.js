072bfcde12be82440c34701630c6b95d59cf0a66
Anthony <73360853+wuuwuuu@users.noreply.github.com>
2023-10-25 22:17:35 -0400

    completed all calculator functionality

---
+++ index.js
@@ -0,0 +1,140 @@
+let display = "";
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+function operate(operatorType) {
+    
+    if (operator == "+" && firstNumber != 0 && secondNumber != 0) {
+        display = parseFloat(firstNumber + secondNumber);
+        setNumbers();
+    }
+    if (operator == "-" && firstNumber != 0 && secondNumber != 0) {
+        display = parseFloat(firstNumber - secondNumber);
+        setNumbers();
+    }
+    if (operator == "*" && firstNumber != 0 && secondNumber != 0) {
+        display = parseFloat(firstNumber * secondNumber);
+        setNumbers();
+    }
+    if (operator == "/" && firstNumber != 0 && secondNumber != 0) {
+        if (firstNumber != 0) {
+            display = parseFloat(firstNumber / secondNumber)
+        } 
+        setNumbers();
+    }
+    function setNumbers() {
+        secondNumber = 0;
+        firstNumber = parseFloat(display);
+    }
+    updateDisplay();
+    operator = operatorType;
+}
+function updateDisplay() {
+    const currentDisplay = document.querySelector('.display');
+    currentDisplay.textContent = display;
+}
+function checkNumbersEmpty() {
+    if (firstNumber == 0 && display != "") {
+        firstNumber = parseFloat(display);
+    }
+    if (firstNumber != 0 && secondNumber == 0 && display == "") {
+        secondNumber = parseFloat(firstNumber);
+    }
+    else if (secondNumber == 0 && display != "" && operator != "") {
+        secondNumber = parseFloat(display);
+    }
+}
+function negative() {
+    display = "-" + display;
+    updateDisplay();
+}
+function percent() {
+    display = parseFloat(display / 100);
+    updateDisplay();
+}
+function evaluateOperation() {
+    checkNumbersEmpty();
+    operate("");
+}
+function add() {  
+    checkNumbersEmpty();
+    operate("+");
+    display = "";
+}
+function subtract() {
+    checkNumbersEmpty();
+    operate("-");
+    display = "";
+}
+function multiply() {
+    checkNumbersEmpty();
+    operate("*");
+    display = "";
+}
+function divide() {
+    checkNumbersEmpty();
+    operate("/");
+    display = "";
+}
+function one() {
+    display += 1;
+    updateDisplay();
+}
+function two() {
+    display += 2;
+    updateDisplay();
+    
+}
+function three() {
+    display += 3;
+    updateDisplay();
+
+}
+function four() {
+    display += 4;
+    updateDisplay();
+
+}
+function five() {
+    display += 5;
+    updateDisplay();
+}
+function six() {
+    display += 6;
+    updateDisplay();
+
+}
+function seven() {
+    display += 7;
+    updateDisplay();
+
+}
+function eight() {
+    display += 8;
+    updateDisplay();
+
+}
+function nine () {
+    display += 9;
+    updateDisplay();
+
+}
+function zero () {
+    display += 0;
+    updateDisplay();
+}
+function decimal() {
+    if (!display.includes(".")) {
+        display += ".";
+        updateDisplay();
+    }
+}
+
+function allClear() {
+    display = "";
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = "";
+    updateDisplay();
+}
